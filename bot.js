b8715a311a7a2fb4f9f84df7ee985917bd07eea6
tawfiqsiam <toto.siam999@gmail.com>
2018-11-11 19:42:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ hours = hours - 12;
 if (hours == 0) {
 hours = 12;
 }
-const w = ['./welcome.png'];
+const w = ['./no2.png'];
               let Image = Canvas.Image,
                   canvas = new Canvas(557, 241),
                   ctx = canvas.getContext('2d');
